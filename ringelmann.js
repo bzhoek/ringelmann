eb7b7356068de2e94a12311ab37ca1ab6c9d8aa7
Bas van der Hoek <bas@hoek.com>
2020-07-24 14:35:00 +0200

    multishape sample as module

--- ringelmann.js
+++ ringelmann.js
@@ -32,3 +32,46 @@ export const makeShape = (points) => {
     .map(segment => segment.map(angle => coordinates(angle, 50))
     .flatMap(point => [point.x, point.y]))
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  const shape = Wilderness.shape
+  const timeline = Wilderness.timeline
+  const render = Wilderness.render
+  const play = Wilderness.play
+
+  const keyframe1 = {
+    type: 'circle',
+    cx: -50,
+    cy: 0,
+    r: 10,
+    fill: '#DBF8A1'
+  }
+
+  const keyframe2 = {
+    type: 'rect',
+    x: 30,
+    y: 10,
+    width: 20,
+    height: 20,
+    fill: '#1F9FFD',
+    duration: 2000
+  }
+
+  const keyframe3 = {
+    type: 'line',
+    x1: 0,
+    y1: -50,
+    x2: 0,
+    y2: 0,
+    fill: '#DBF8A1',
+    duration: 2000
+  }
+
+  const morph = shape(keyframe1, keyframe2, keyframe3)
+  const animation = timeline(morph)
+
+  render(document.querySelector('svg'), animation)
+
+  play(animation)
+})
